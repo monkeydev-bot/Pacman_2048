4a3cac9c60a7d8974755667a3c744d99236adee5
GaspardVCS <45163925+GaspardVCS@users.noreply.github.com>
2018-11-19 15:44:16 +0100

    Update move_ennemi.py

--- move_ennemi.py
+++ move_ennemi.py
@@ -1,32 +1,52 @@
-import random
-
-def new_position_ennemi():
-    ligne_ennemi, colonne_ennemi = get_position_ennemi()
-    ligne_player, colonne_player = get_position_player()
+def move_pos_ennemi(position_player, position_ennemi, position_objectif):
+    ligne_ennemi, colonne_ennemi = position_ennemi
+    ligne_player, colonne_player = position_player
+    ligne_objectif, colonne_objectif = position_objectif
     if ligne_ennemi == ligne_player :
-        if colonne_player < colonne_ennemi :
+        if (colonne_player < colonne_ennemi) and (ligne_ennemi != ligne_objectif or colonne_ennemi != colonne_objectif + 1 ) :
             colonne_ennemi -= 1
-        else:
+        elif (colonne_player < colonne_ennemi) and (ligne_ennemi == ligne_objectif and colonne_ennemi == colonne_objectif + 1 ):
+            colonne_ennemi -= 2
+        elif (colonne_player > colonne_ennemi) and (ligne_ennemi != ligne_objectif or colonne_ennemi != colonne_objectif - 1 ):
             colonne_ennemi += 1
+        else:
+            colonne_ennemi += 2
     elif colonne_ennemi == colonne_player:
-        if ligne_player < ligne_ennemi :
+        if (ligne_player < ligne_ennemi) and (colonne_ennemi != colonne_objectif or ligne_ennemi != ligne_objectif + 1 ) :
+            ligne_ennemi -= 1
+        elif (ligne_player < ligne_ennemi) and (colonne_ennemi == colonne_objectif and ligne_ennemi == ligne_objectif + 1 ):
+            ligne_ennemi -= 2
+        elif (ligne_player > ligne_ennemi) and (colonne_ennemi != colonne_objectif or ligne_ennemi != ligne_objectif - 1 ):
+            ligne_ennemi += 1
+        else:
+            ligne_ennemi += 2
+    elif (colonne_player > colonne_ennemi) and (ligne_ennemi != ligne_objectif or colonne_ennemi!=colonne_objectif -1):
+        colonne_ennemi+= 1
+    elif (colonne_player > colonne_ennemi) and (ligne_ennemi == ligne_objectif or colonne_ennemi==colonne_objectif -1):
+        if (ligne_player > ligne_ennemi):
+            ligne_ennemi+=1
+        else:
+            ligne_ennemi-= 1
+    elif (colonne_player < colonne_ennemi) and (ligne_ennemi != ligne_objectif or colonne_ennemi!=colonne_objectif + 1):
+        colonne_ennemi -= 1
+    elif (colonne_player < colonne_ennemi) and (ligne_ennemi == ligne_objectif and colonne_ennemi==colonne_objectif + 1):
+        if (ligne_player > ligne_ennemi):
             ligne_ennemi += 1
         else :
             ligne_ennemi -= 1
-    else:
-        if ligne_player < ligne_ennemi :
-            ligne_ennemi -= 1
-        else:
-            ligne_ennemi += 1
 
     return ligne_ennemi, colonne_ennemi
 
-def move_ennemi(grid):
-    ligne_ennemi, colonne_ennemi = get_position_ennemi()
-    new_ligne_ennemi, new_colonne_ennemi = new_position_ennemi()
-    grid[ligne_ennemi][colonne_ennemi] = 0
-    grid[new_ligne_ennemi][new_colonne_ennemi] = int(value_objectif(grid)/4)
 
-def test_new_position_ennemi():
+def test_move_pos_ennemi():
+    assert move_pos_ennemi((1,1),(5,1),(3,4))== (4,1)
+    assert move_pos_ennemi((1,1), (5,1), (4,1))== (3,1)
+    assert move_pos_ennemi((2,1), (2,6), (1,5))== (2,5)
+    assert move_pos_ennemi((2,1), (2,6), (2,5))== (2,4)
+    assert move_pos_ennemi((1,1), (5,5), (3,3))== (5,4)
+    assert move_pos_ennemi((1,1), (5,5), (5,4))== (4,5)
+    assert move_pos_ennemi((7,0), (1,5), (2,4))== (1,4)
+    assert move_pos_ennemi((7,0), (1,5), (1,4))== (2,5)
+    assert move_pos_ennemi((2,3), (4,3), (3,3))== (2,3)
 
-    assert 
+test_move_pos_ennemi()
