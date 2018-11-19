2f57296731546d223a284f57e43ad3effa740f57
Adri98 <45163889+Adri98@users.noreply.github.com>
2018-11-19 16:02:50 +0100

    Add files via upload

---
+++ grid_to_string_with_size.py
@@ -0,0 +1,33 @@
+
+
+def creategrid(n):
+    grid=[]
+    for i in range(n):
+        grid.append([0]*n)
+    return grid
+
+
+def grid_to_string(grid):
+    
+    THEMES = {
+        "0": {"name": "Default", 1:"1",  0: " ", 2: "2", 4: "4", 8: "8", 16: "16", 32: "32", 64: "64", 128: "128", 256: "256",
+              512: "512", 1024: "1024", 2048: "2048", 4096: "4096", 8192: "8192"},
+        }
+   
+    
+    size = len(grid)
+    string = """"""
+    entre_lignes=" "+("="*4+" ")*size + "\n"
+    string+=entre_lignes
+    for i in range(size):
+        string += "|"
+        for j in range(size):
+            ch=THEMES["0"][grid[i][j]]
+            while len(ch)<4:
+                ch+=" "
+            string+=ch
+            string+="|"
+        string+="\n"
+        string+=entre_lignes
+    
+    return(string)
