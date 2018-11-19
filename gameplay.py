83cba14750c76d037e094dd34dfa6788426d9058
walidtahiriwt <45100696+walidtahiriwt@users.noreply.github.com>
2018-11-19 15:53:58 +0100

    Add files via upload

--- gameplay.py
+++ gameplay.py
@@ -1,6 +1,6 @@
 from newgame2048.init_game import get_new_position
 
-def player_sur_objectif(grid,pos_player,pos_ennemy,pos_objectif):
+def player_sur_objectif(grid, pos_player,pos_ennemi, pos_objectif):
     i0,j0=pos_player
     grid[i0][j0]=0
     pos_player=pos_objectif
@@ -8,7 +8,7 @@ def player_sur_objectif(grid,pos_player,pos_ennemy,pos_objectif):
     new_abs_player,new_ord_player=pos_player
     value_player=grid[new_abs_player][new_ord_player]
     grid[i1][j1]=2*value_player
-    abs_ennemy,ord_ennemy=pos_ennemy
+    abs_ennemy,ord_ennemy=pos_ennemi
     grid[abs_ennemy][ord_ennemy]=value_player/2
     return grid
 
@@ -19,9 +19,9 @@ def move_player(grid,pos_player,commande):
     grid[old_abs_player][old_ord_player]=0
     return grid
 
-def move_ennemy(grid,pos_ennemy,pos_objectif,commande):
-    old_abs_ennemy, old_ord_ennemy=pos_ennemy
-    new_abs_ennemy, new_ord_ennemy=move_pos_ennemy(pos_ennemy,pos_objectif,commande)
-    grid[new_abs_ennemy][new_ord_ennemy]=grid[old_abs_ennemy][old_ord_ennemy]
-    grid[old_abs_ennemy][old_ord_ennemy]=0
+def move_ennemi(grid,pos_ennemi, pos_objectif, commande):
+    old_abs_ennemi, old_ord_ennemi=pos_ennemi
+    new_abs_ennemi, new_ord_ennemi=move_pos_ennemi(pos_ennemi, pos_objectif, commande)
+    grid[new_abs_ennemi][new_ord_ennemi]=grid[old_abs_ennemi][old_ord_ennemi]
+    grid[old_abs_ennemi][old_ord_ennemi]=0
     return grid
