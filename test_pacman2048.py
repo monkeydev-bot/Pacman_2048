cac4f276df5a332db175d686bc1738e268b8c724
GaspardVCS <45163925+GaspardVCS@users.noreply.github.com>
2018-11-22 09:44:45 +0100

    Update test_pacman2048.py

--- test_pacman2048.py
+++ test_pacman2048.py
@@ -13,8 +13,8 @@ def test_move_ennemi():
     assert move_ennemi([[0, 0, 0, 0, 0], [0, 0, 0, 8, 0], [0, 0, 0, 0, 0], [0, 4, 0, 2, 0], [0, 0, 0, 0, 0]],(3,1), (3,3),(1,3)) == [[0, 0, 0, 0, 0], [0, 0, 0, 8, 0], [0, 0, 0, 0, 0], [0, 4, 2, 0, 0], [0, 0, 0, 0, 0]]
 
 def test_create_grid():
-    assert grid(4) ==[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-    assert grid(6) == [[0 for i in range(6)] for j in range(6)]
+    assert create_grid(4) ==[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    assert create_grid(6) == [[0 for i in range(6)] for j in range(6)]
 
 def test_grid_add_new_tile_at_position():
     assert grid_add_new_tile_at_position([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], 1,3,4) == [[0, 0, 0, 0, 0], [0, 0, 0, 16, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
@@ -26,3 +26,24 @@ def test_get_empty_tiles_positions():
 def test_get_new_position():
     assert get_new_position([[0,2,6],[5,0,7],[2,2,2]]) in [(0,0),(1,1)]
 
+def test_init_game():
+    assert init_game(4)[3][0] == 2
+    assert len(init_game(10))== 10
+
+def test_long_value():
+    assert long_value([[2,56],[4,2067]])== 4
+    assert long_value([[3,45],[2,0]]) == 2
+
+def test_move_pos_ennemi():
+    assert move_pos_ennemi((1,1),(5,1),(3,4))== (4,1)
+    assert move_pos_ennemi((1,1), (5,1), (4,1))== (3,1)
+    assert move_pos_ennemi((2,1), (2,6), (1,5))== (2,5)
+    assert move_pos_ennemi((2,1), (2,6), (2,5))== (2,4)
+    assert move_pos_ennemi((1,1), (5,5), (3,3))== (5,4)
+    assert move_pos_ennemi((1,1), (5,5), (5,4))== (4,5)
+    assert move_pos_ennemi((7,0), (1,5), (2,4))== (1,4)
+    assert move_pos_ennemi((7,0), (1,5), (1,4))== (2,5)
+    assert move_pos_ennemi((2,3), (4,3), (3,3))== (2,3)
+
+def test_get_position_player():
+    assert get_position_player([[0, 2, 0, 0, 0], [0, 0, 4, 0, 0], [0, 0, 0, 8, 0], [0, 0, 0, 0, 0]]) == (1,2)
